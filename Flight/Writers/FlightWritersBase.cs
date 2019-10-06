0de1d5f9f4505141c777a9b4c360d67f2508f644
Michael <mejevin@gmail.com>
2019-10-06 22:51:36 +0300

    add: little comment

--- Flight/Writers/FlightWritersBase.cs
+++ Flight/Writers/FlightWritersBase.cs
@@ -5,7 +5,7 @@ namespace Flight.Writers
     /* 
      * This is an interface that is used to output flight info somewhere, or somehow
      * 
-     * It could be a console, a UI component, a DB entry or anything that requires output
+     * It could be a console, a UI component, a DB entry, a file or anything that requires output
      * 
      * Works in conjunction with an implementation of IFlightSerializer that optionally
      * serializes the input flight data
