3c04398d728dd4ba2eb7b9e9bc41d2f86bcbf0cf
Michael <mejevin@gmail.com>
2019-10-07 23:21:20 +0300

    fix: Comment typo

--- Flight/Validators/FlightValidatorsBase.cs
+++ Flight/Validators/FlightValidatorsBase.cs
@@ -3,7 +3,7 @@ namespace Flight.Validators
 {
     public interface IFlightValidator
     {
-        // True if we want to keep the flight, false if we want to keep it
+        // True if we want to discard the flight, false if we want to keep it
         bool Discard(Flight flight);
     }
 }
